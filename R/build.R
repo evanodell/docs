5bb01eddd9747628b12fd6637e2a269e2ca3fd5a
EvanOdell <evanodell91@gmail.com>
2017-05-08 13:48:48 +0100

    moving files to docs folder

--- R/build.R
+++ R/build.R
@@ -2,13 +2,11 @@
 
 
 
-#' Title
+#' build_docs
+#' 
+#' Build documentation for docs.evanodell.com
 #'
-#' @return
-#' @export
-#'
-#' @examples
-build_docs <- function(other=NULL){
+build_docs <- function(){
 
 ### Build `hansard`
 pkgdown::build_site(pkg = "/Users/evanodell/Documents/GitHub/hansard", path= "/Users/evanodell/Documents/GitHub/docs/hansard")
@@ -19,11 +17,7 @@ pkgdown::build_site(pkg = "/Users/evanodell/Documents/GitHub/mnis", path= "/User
 ### Build `parlitools`
 pkgdown::build_site(pkg = "/Users/evanodell/Documents/GitHub/parlitools", path= "/Users/evanodell/Documents/GitHub/docs/parlitools")
 
-### If other packages to build for:
-
-
-
-pkgdown::build_site(path = ".")
+pkgdown::build_site()
 
 }
 
@@ -32,11 +26,7 @@ pkgdown::build_site(path = ".")
 #' Title
 #'
 #' @param other The name of another R packages to build documentation for. Can only accept one package at a time. Must be a character vector, and equal to the folder name of the repository in the local GitHub folder.
-#'
-#' @return
-#' @export
-#'
-#' @examples
+
 
 other_docs <- function(other=NULL){
   
