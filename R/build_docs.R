381c4301a96dcd5a2cf1c86de63dcd65c5ef6c49
EvanOdell <evanodell91@gmail.com>
2017-07-18 12:52:18 +0000

    update documentation

--- R/build_docs.R
+++ R/build_docs.R
@@ -3,18 +3,25 @@
 
 
 #' build_docs
-#' Build documentation for docs.evanodell.com
+#' 
+#' Build documentation for R packages for for docs.evanodell.com
 #' @export
 build_docs <- function(){
 
 ### Build `hansard`
   pkgdown::build_site(pkg = "/Users/evanodell/Documents/GitHub/hansard", path= "/Users/evanodell/Documents/GitHub/docs/hansard", preview = FALSE)
+  
+  ### Build `hansard-ropengov`
+  pkgdown::build_site(pkg = "/Users/evanodell/Documents/GitHub/hansard-rOpenGov", path= "/Users/evanodell/Documents/GitHub/hansard-rOpenGov/docs", preview = FALSE)
 
 ### Build `mnis`
   pkgdown::build_site(pkg = "/Users/evanodell/Documents/GitHub/mnis", path= "/Users/evanodell/Documents/GitHub/docs/mnis", preview = FALSE)
 
 ### Build `parlitools`
   pkgdown::build_site(pkg = "/Users/evanodell/Documents/GitHub/parlitools", path= "/Users/evanodell/Documents/GitHub/docs/parlitools", preview = FALSE)
+  
+  ### Build `emisc`
+  pkgdown::build_site(pkg = "/Users/evanodell/Documents/GitHub/emisc", path= "/Users/evanodell/Documents/GitHub/docs/emisc", preview = FALSE)
 
   pkgdown::build_site(path=".", preview = FALSE)
 
@@ -23,6 +30,8 @@ build_docs <- function(){
 
 
 #' other_docs
+#' 
+#' Function to build documentation for other R packages not listed in \code{\link{build_docs}}.
 #' @param pkg Location of files for input
 #' @param path Output location, defaults to this folder.
 #' @param other The name of another R packages to build documentation for.
@@ -30,11 +39,14 @@ build_docs <- function(){
 other_docs <- function(pkg="/Users/evanodell/Documents/GitHub/", path="/Users/evanodell/Documents/GitHub/docs/", other=NULL){
   
     pkgdown::build_site(pkg = paste0(pkg, other), path= paste0(path, other))
+  
 }
 
 
 
 #' build_docs_home
+#'
+#' Build the homepage of docs.evanodell.com
 #'
 #' @export
 build_docs_home <- function(){
