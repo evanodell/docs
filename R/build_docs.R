b50dc0b72b6d7d339802fa3aadd24641973af75e
Evan Odell <evanodell91@gmail.com>
2018-03-13 10:54:39 +0000

    update doc navigation

--- R/build_docs.R
+++ R/build_docs.R
@@ -4,213 +4,31 @@
 #' Build documentation for R packages for for docs.evanodell.com
 #' @export
 build_docs <- function(){
-
-  message("Building `hansard`")
   
 ### Build `hansard`  -----------------
-  pkgdown::build_site(pkg = "/Users/evanodell/Documents/GitHub/hansard", 
-                      path = "/Users/evanodell/Documents/GitHub/docs/hansard",
-                      preview = FALSE)
   
-  hansard_doc_files <- list.files("/Users/evanodell/Documents/GitHub/hansard/docs", 
-                              all.files = TRUE, full.names = TRUE, 
-                              recursive = TRUE, ignore.case = TRUE, 
-                              include.dirs = TRUE, no.. = TRUE)
-  
-  
-  if (!dir.exists("hansard")) {
-    dir.create("hansard")
-  }
-  
-  file.copy(hansard_doc_files, 
-            "/Users/evanodell/Documents/GitHub/docs/hansard", 
-            overwrite = TRUE, recursive = TRUE)
-  
-  hansard_dupes <- list.files(path = "hansard/", pattern = "*.html")
-  hansard_refs <- list.files(path = "hansard/reference", pattern = "*.html")
-  
-  hansard_refs <- hansard_refs[hansard_refs != "index.html"]
-  
-  hansard_dupes <- as.list(
-    paste0("hansard/", hansard_dupes[hansard_dupes %in% hansard_refs])
-    )
-  
-  hansard_dupes[[length(hansard_dupes)+1]] <- "hansard/introduction.R"
-  hansard_dupes[[length(hansard_dupes)+2]] <- "hansard/introduction.html"
-  
-  do.call(file.remove, hansard_dupes)
-  
-  
-  ropengov_doc_files <- list.files("/Users/evanodell/Documents/GitHub/hansard", 
-                              all.files = TRUE, full.names = TRUE, 
-                              recursive = FALSE, ignore.case = TRUE, 
-                              include.dirs = FALSE, no.. = TRUE)
-  
-  ropengov_doc_files <- ropengov_doc_files[ !grepl("_pkgdown.yml", ropengov_doc_files)]
-                                  
-  ropengov_doc_files <- ropengov_doc_files[ !grepl(".DS_Store", ropengov_doc_files) ]
-  
-  ropengov_doc_files <- ropengov_doc_files[ !grepl("hansard.Rproj", ropengov_doc_files) ]
-  
-  file.copy(ropengov_doc_files, "/Users/evanodell/Documents/GitHub/hansard-rOpenGov", 
-            overwrite = TRUE)
-  
-  file.copy("/Users/evanodell/Documents/GitHub/hansard/tests", 
-            "/Users/evanodell/Documents/GitHub/hansard-rOpenGov", 
-            overwrite = TRUE, recursive = TRUE)
-  
-  file.copy("/Users/evanodell/Documents/GitHub/hansard/R/", 
-            "/Users/evanodell/Documents/GitHub/hansard-rOpenGov", 
-            overwrite = TRUE, recursive = TRUE)
-  
-  file.copy("/Users/evanodell/Documents/GitHub/hansard/man", 
-            "/Users/evanodell/Documents/GitHub/hansard-rOpenGov", 
-            overwrite = TRUE, recursive = TRUE)
-  
-  file.copy("/Users/evanodell/Documents/GitHub/hansard/inst", 
-            "/Users/evanodell/Documents/GitHub/hansard-rOpenGov", 
-            overwrite = TRUE, recursive = TRUE)
-  
-  file.copy("/Users/evanodell/Documents/GitHub/hansard/vignettes",
-            "/Users/evanodell/Documents/GitHub/hansard-rOpenGov", 
-            overwrite = TRUE, recursive = TRUE)
-  
-  message("Building `hansard-ropengov`")
+  docs::build_hansard()
   
 ### Build `hansard-ropengov` -----------------
-  pkgdown::build_site(pkg = "/Users/evanodell/Documents/GitHub/hansard-rOpenGov", 
-                      path = "/Users/evanodell/Documents/GitHub/hansard-rOpenGov/docs", 
-                      preview = FALSE)
-
-  message("Building `mnis`")
+  
+  docs::build_hansard_opengov()
   
 ### Build `mnis` -----------------
-  pkgdown::build_site(pkg = "/Users/evanodell/Documents/GitHub/mnis", 
-                      preview = FALSE)
+  
+  docs::build_mnis()
+  
+### Build `parlitools` -----------------  
   
   
-  mnis_doc_files <- list.files("/Users/evanodell/Documents/GitHub/mnis/docs", 
-                                  all.files = TRUE, full.names = TRUE, 
-                                  recursive = TRUE, ignore.case = TRUE, 
-                                  include.dirs = TRUE, no.. = TRUE)
-  
-  if (!dir.exists("mnis")) {
-    dir.create("mnis")
-  }
-  
-  file.copy(mnis_doc_files, 
-            "/Users/evanodell/Documents/GitHub/docs/mnis", 
-            recursive = TRUE)
-  
-  mnis_dupes <- list.files(path = "mnis/", pattern = "*.html")
-  mnis_refs <- list.files(path = "mnis/reference", pattern = "mnis_.*.html")
-  
-  mnis_dupes <- as.list(paste0("mnis/", mnis_dupes[mnis_dupes %in% mnis_refs]))
-  
-  mnis_dupes[[length(mnis_dupes)+1]] <- "mnis/introduction.R"
-  mnis_dupes[[length(mnis_dupes)+2]] <- "mnis/introduction.html"
-  
-  do.call(file.remove, mnis_dupes)
-  
-  message("Building `parlitools`")
-  
-### Build `parlitools` -----------------
-  pkgdown::build_site(pkg = "/Users/evanodell/Documents/GitHub/parlitools", 
-                      path = "/Users/evanodell/Documents/GitHub/docs/parlitools", 
-                      preview = FALSE)
-  
-  parlitools_doc_files <- list.files(
-    "/Users/evanodell/Documents/GitHub/parlitools/docs",
-    all.files = TRUE, full.names = TRUE, recursive = TRUE, 
-    ignore.case = TRUE, include.dirs = TRUE, no.. = TRUE)
-  
-  if (!dir.exists("parlitools")) {
-    dir.create("parlitools")
-  }
-  
-  file.copy(parlitools_doc_files, 
-            "/Users/evanodell/Documents/GitHub/docs/parlitools", 
-            overwrite = TRUE, recursive = TRUE)
-  
-  parlitools_dupes <- list.files(path = "parlitools/", pattern = "*.html")
-  parlitools_refs <- list.files(path = "parlitools/reference", pattern = "*.html")
-  
-  parlitools_refs <- parlitools_refs[parlitools_refs != "index.html"]
-  
-  parlitools_dupes_list <- as.list(
-    paste0("parlitools/", 
-           parlitools_dupes[parlitools_dupes %in% parlitools_refs])
-  )
-  
-  parlitools_arts <- list.files(path = "parlitools/articles", pattern = "*.html")
-  
-  parlitools_arts <- parlitools_arts[parlitools_arts != "index.html"]
-  
-  # parlitools_arts2 <- list.files(path = "parlitools", pattern = "*_files")
-  # 
-  # parlitools_arts2 <- parlitools_arts2[parlitools_arts2 != "index_files"]
-  
-  parlitools_dupes_list2 <- as.list(
-    paste0("parlitools/", 
-           parlitools_dupes[parlitools_dupes %in% parlitools_arts])
-  )
-  
-  parlitools_dupes <- c(parlitools_dupes_list, parlitools_dupes_list2)
-  
-  do.call(file.remove, parlitools_dupes)
-  
-  #unlink(parlitools_arts2, recursive = TRUE)
-  
-  
-  message("Building `emisc`")
+  docs::build_parlitools()
   
 ### Build `emisc`  -----------------
-  pkgdown::build_site(pkg = "/Users/evanodell/Documents/GitHub/emisc", 
-                      path = "/Users/evanodell/Documents/GitHub/docs/emisc", 
-                      preview = FALSE)
   
-  emisc_doc_files <- list.files(
-    "/Users/evanodell/Documents/GitHub/emisc/docs",
-    all.files = TRUE, full.names = TRUE, recursive = TRUE, 
-    ignore.case = TRUE, include.dirs = TRUE, no.. = TRUE)
-  
-  if (!dir.exists("emisc")) {
-    dir.create("emisc")
-  }
-  
-  file.copy(emisc_doc_files, 
-            "/Users/evanodell/Documents/GitHub/docs/emisc", 
-            overwrite = TRUE, recursive = TRUE)
-  
+  docs::build_emisc()
   
 ### Build `nomisr`  -----------------
   
-  pkgdown::build_site(pkg = "/Users/evanodell/Documents/GitHub/nomisr", 
-                      path = "/Users/evanodell/Documents/GitHub/docs/nomisr", 
-                      preview = FALSE)
-  
-  nomisr_doc_files <- list.files(
-    "/Users/evanodell/Documents/GitHub/nomisr/docs",
-    all.files = TRUE, full.names = TRUE, recursive = TRUE, 
-    ignore.case = TRUE, include.dirs = TRUE, no.. = TRUE)
-  
-  if (!dir.exists("nomisr")) {
-    dir.create("nomisr")
-  }
-  
-  file.copy(nomisr_doc_files, 
-            "/Users/evanodell/Documents/GitHub/docs/nomisr", 
-            recursive = TRUE)
-  
-  nomisr_dupes <- list.files(path = "nomisr/", pattern = "*.html")
-  nomisr_refs <- list.files(path = "nomisr/reference", pattern = "nomis_.*.html")
-  
-  nomisr_dupes <- as.list(paste0("nomisr/", nomisr_dupes[nomisr_dupes %in% nomisr_refs]))
-  
-  nomisr_dupes[[length(nomisr_dupes)+1]] <- "nomisr/introduction.R"
-  nomisr_dupes[[length(nomisr_dupes)+2]] <- "nomisr/introduction.html"
-  
-  do.call(file.remove, nomisr_dupes)
+  docs::build_nomisr()
 
   # pkgdown::build_site(path= "/", preview = FALSE)
   # 
@@ -222,6 +40,10 @@ build_docs <- function(){
   # file.copy("/Users/evanodell/Documents/GitHub/docs/docs", 
   #           "/Users/evanodell/Documents/GitHub/docs", 
   #           recursive = TRUE, overwrite = TRUE)
+  
+### Building homepage -----------
+  
+  docs::build_docs_home()
 
 }
 
@@ -252,6 +74,16 @@ other_docs <- function(pkg="/Users/evanodell/Documents/GitHub/",
 #' @export
 build_docs_home <- function(){
   pkgdown::build_site(path=".", preview = FALSE)
+  
+  doc_files <- list.files("docs", 
+                               all.files = TRUE, full.names = TRUE, 
+                               recursive = FALSE, ignore.case = TRUE, 
+                               include.dirs = TRUE, no.. = TRUE)
+  
+  file.copy(doc_files, 
+            ".", 
+            recursive = TRUE)
+  
 }
 
 
---
+++ R/build_packages.R
@@ -0,0 +1,160 @@
+
+#' @export
+build_nomisr <- function(){
+  
+  pkgdown::build_site(pkg = "/Users/evanodell/Documents/GitHub/nomisr", 
+                      preview = FALSE)
+  
+  nomisr_doc_files <- list.files(
+    "/Users/evanodell/Documents/GitHub/nomisr/docs",
+    all.files = TRUE, full.names = TRUE, recursive = TRUE, 
+    ignore.case = TRUE, include.dirs = TRUE, no.. = TRUE)
+  
+  unlink("nomisr", recursive = TRUE)
+  dir.create("nomisr")
+  
+  file.copy(nomisr_doc_files, 
+            "/Users/evanodell/Documents/GitHub/docs/nomisr", 
+            recursive = TRUE)
+  
+}
+
+#' @export
+build_hansard <- function(){
+  
+  message("Building `hansard`")
+  
+  pkgdown::build_site(pkg = "/Users/evanodell/Documents/GitHub/hansard", 
+                      path = "/Users/evanodell/Documents/GitHub/docs/hansard",
+                      preview = FALSE)
+  
+  unlink("hansard", recursive = TRUE)
+  
+  dir.create("hansard")
+  
+  hansard_doc_files <- list.files(
+    "/Users/evanodell/Documents/GitHub/hansard/docs", 
+    all.files = TRUE, full.names = TRUE, 
+    recursive = F, ignore.case = TRUE, 
+    include.dirs = TRUE, no.. = TRUE)
+  
+  file.copy(hansard_doc_files, "hansard", recursive = TRUE)
+  
+}
+
+#' @export
+build_hansard_opengov <- function(){
+  
+  message("Building `hansard-ropengov`")
+  
+  ropengov_doc_files <- list.files("/Users/evanodell/Documents/GitHub/hansard", 
+                                   all.files = TRUE, full.names = TRUE, 
+                                   recursive = FALSE, ignore.case = TRUE, 
+                                   include.dirs = FALSE, no.. = TRUE)
+  
+  ropengov_doc_files <- ropengov_doc_files[ !grepl("_pkgdown.yml", ropengov_doc_files)]
+  
+  ropengov_doc_files <- ropengov_doc_files[ !grepl(".DS_Store", ropengov_doc_files) ]
+  
+  ropengov_doc_files <- ropengov_doc_files[ !grepl("hansard.Rproj", ropengov_doc_files) ]
+  
+  file.copy(ropengov_doc_files, "/Users/evanodell/Documents/GitHub/hansard-rOpenGov", 
+            overwrite = TRUE)
+  
+  file.copy("/Users/evanodell/Documents/GitHub/hansard/tests", 
+            "/Users/evanodell/Documents/GitHub/hansard-rOpenGov", 
+            overwrite = TRUE, recursive = TRUE)
+  
+  file.copy("/Users/evanodell/Documents/GitHub/hansard/R/", 
+            "/Users/evanodell/Documents/GitHub/hansard-rOpenGov", 
+            overwrite = TRUE, recursive = TRUE)
+  
+  file.copy("/Users/evanodell/Documents/GitHub/hansard/man", 
+            "/Users/evanodell/Documents/GitHub/hansard-rOpenGov", 
+            overwrite = TRUE, recursive = TRUE)
+  
+  file.copy("/Users/evanodell/Documents/GitHub/hansard/inst", 
+            "/Users/evanodell/Documents/GitHub/hansard-rOpenGov", 
+            overwrite = TRUE, recursive = TRUE)
+  
+  file.copy("/Users/evanodell/Documents/GitHub/hansard/vignettes",
+            "/Users/evanodell/Documents/GitHub/hansard-rOpenGov", 
+            overwrite = TRUE, recursive = TRUE)
+  
+  
+  pkgdown::build_site(pkg = "/Users/evanodell/Documents/GitHub/hansard-rOpenGov", 
+                      path = "/Users/evanodell/Documents/GitHub/hansard-rOpenGov/docs", 
+                      preview = FALSE)
+  
+}
+
+
+#' @export
+build_parlitools <- function(){
+  
+  message("Building `parlitools`")
+  
+  pkgdown::build_site(pkg = "/Users/evanodell/Documents/GitHub/parlitools", 
+                      preview = FALSE)
+  
+  parlitools_doc_files <- list.files(
+    "/Users/evanodell/Documents/GitHub/parlitools/docs", 
+    all.files = TRUE, full.names = TRUE, 
+    recursive = FALSE, ignore.case = TRUE, 
+    include.dirs = TRUE, no.. = TRUE)
+  
+  unlink("parlitools", recursive = TRUE)
+  dir.create("parlitools")
+  
+  file.copy(parlitools_doc_files, 
+            "/Users/evanodell/Documents/GitHub/docs/parlitools", 
+            recursive = TRUE)
+  
+  
+}
+
+
+#' @export
+build_emisc <- function(){
+  
+  message("Building `emisc`")
+  
+  pkgdown::build_site(pkg = "/Users/evanodell/Documents/GitHub/emisc", 
+                      path = "/Users/evanodell/Documents/GitHub/docs/emisc", 
+                      preview = FALSE)
+  
+  emisc_doc_files <- list.files(
+    "/Users/evanodell/Documents/GitHub/emisc/docs",
+    all.files = TRUE, full.names = TRUE, recursive = FALSE, 
+    ignore.case = TRUE, include.dirs = TRUE, no.. = TRUE)
+  
+  unlink("emisc", recursive = TRUE)
+  dir.create("emisc")
+  
+  file.copy(emisc_doc_files, 
+            "/Users/evanodell/Documents/GitHub/docs/emisc", 
+            recursive = TRUE)
+  
+}
+
+#' @export
+build_mnis <- function(){
+  
+  message("Building `mnis`")
+  
+  pkgdown::build_site(pkg = "/Users/evanodell/Documents/GitHub/mnis", 
+                      preview = FALSE)
+  
+  mnis_doc_files <- list.files("/Users/evanodell/Documents/GitHub/mnis/docs", 
+                               all.files = TRUE, full.names = TRUE, 
+                               recursive = FALSE, ignore.case = TRUE, 
+                               include.dirs = TRUE, no.. = TRUE)
+  
+  unlink("mnis", recursive = TRUE)
+  dir.create("mnis")
+  
+  file.copy(mnis_doc_files, 
+            "/Users/evanodell/Documents/GitHub/docs/mnis", 
+            recursive = TRUE)
+  
+}
